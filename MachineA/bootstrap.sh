f06c0c10d73097c14b6fa87adb92110c3a2933d2
Mateusz Krawczyk <krawat10@gmail.com>
2021-01-19 20:53:50 +0100

    First commit

---
+++ MachineA/bootstrap.sh
@@ -0,0 +1,3 @@
+apt-get update
+apt-get install -y lldpd
+/etc/init.d/lldpd start
---
+++ SwitchRight/bootstrap.sh
@@ -0,0 +1,42 @@
+apt-get update
+
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-3.1
+
+sudo apt-get install -y dotnet-runtime-3.1
+
+
+sudo apt-get install -y aptitude
+sudo aptitude install -y tcpdump
+sudo aptitude install -y bridge-utils
+sudo aptitude install -y vlan
+sudo aptitude install -y tmux
+sudo aptitude install -y git
+sudo aptitude install -y gnupg
+
+#SNMP
+sudo aptitude install snmpd snmp libsnmp-dev -y
+sudo systemctl stop snmpd
+net-snmp-create-v3-user -ro -A STrP@SSWRD -a SHA -X STr0ngP@SSWRD -x AES snmpro
+sudo systemctl start snmpd
+sudo snmpwalk -v3 -a SHA -A STrP@SSWRD -x AES -X STr0ngP@SSWRD -l authPriv -u snmpro localhost | head -5
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo git clone --depth 1 https://github.com/krawat10/EthernetSwitch.git
+sudo dotnet publish -c Release EthernetSwitch/EthernetSwitch
+#sudo sed -i 's/vagrant/krawat/g' EthernetSwitch/EthernetSwitch/ethernet-switch.service
+sudo cp EthernetSwitch/EthernetSwitch/ethernet-switch.service /etc/systemd/system/ethernet-switch.service
+sudo systemctl daemon-reload
+sudo systemctl enable ethernet-switch.service
+sudo systemctl start ethernet-switch.service
